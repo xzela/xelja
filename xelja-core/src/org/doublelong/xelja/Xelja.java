b81c7b71bb1ecb66ca8a80e537c70a3167ba3a74
Zeph Lafassett <zeph@lafassett.com>
2013-10-06 12:59:04 -0700

    added initial class files

---
+++ xelja-core/src/org/doublelong/xelja/Xelja.java
@@ -0,0 +1,8 @@
+package org.doublelong.xelja;
+
+public class Xelja
+{
+	public static final float WINDOW_HEIGHT = 800;
+	public static final float WINDOW_WIDTH = 800;
+	public static final String WINDOW_TITLE = "Xelja - the adventure";
+}
